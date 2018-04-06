1386a3104d8402e8944d0c6e386aee9d6cdc7d3b
CreepPork <creepporkstudios@gmail.com>
2018-04-07 00:08:47 +0300

    Added broken signature verification

--- karmanor.py
+++ karmanor.py
@@ -1,14 +1,39 @@
+# Import Python libraries
 import logging
+import json
+import hashlib
+import os
+import hmac
 
+# Import 3rd party libraries
 from flask import Flask, request, Response
 import requests
+
+# Import project libraries
+import settings
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["POST"])
 def index():
-    return "Hello World!"
+
+    GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+    mac = hmac.new(bytes(GITHUB_TOKEN, 'utf-8'),
+                   msg=request.data, digestmod='sha1')
+
+    signature = request.headers.get('X-Hub-Signature')
+
+    print(mac.hexdigest())
+    print(signature.split('=')[1])
+
+    isRequestFromGitHub = hmac.compare_digest(
+        str(mac.hexdigest()), str(signature.split('=')[1]))
+
+    data = request.get_json()
+
+    return str(isRequestFromGitHub)
 
 
 @app.errorhandler(500)
@@ -23,4 +48,4 @@ def server_error(e):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
