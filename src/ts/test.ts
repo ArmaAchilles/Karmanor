245d664fcd4c9f1652380c611c40998968a68e1c
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-04-13 22:54:44 +0300

    Added only structure for Test class

---
+++ src/ts/test.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default class Test {
+    requests(fail?: false) {
+        // Generate some zip file
+
+        // Start server if not started
+
+        // Make POST request to itself with zip (if fail then provide a random token)
+
+        // If set to fail check that the zip got deleted
+
+        // If fail is false then check that the zip got extracted
+
+        // Check if the game got launched with the right params
+
+        // Check if the RPT is getting read by providing bogus build failed texts
+
+        // Check for cleanup of zip
+
+        // Check that build failed
+
+        // Return true if all went well
+    }
+}
