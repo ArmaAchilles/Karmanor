b0bdca5cfd25341bc9f13a8812fa00de33ffe7ce
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-04-14 12:33:10 +0300

    Moved building from dist/ to build/

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,13 +12,13 @@ mix.options({
     processCssUrls: false
 });
 
-mix.ts('src/ts/main.ts', 'dist/js')
-    .ts('src/ts/app.ts', 'dist/js')
-    .sass('src/sass/app.scss', 'dist/css', {
+mix.ts('src/ts/main.ts', 'build/js')
+    .ts('src/ts/app.ts', 'build/js')
+    .sass('src/sass/app.scss', 'build/css', {
         includePaths: ['node_modules']
     })
-    .copy('src/html', 'dist/html', false)
-    .copy('src/images', 'dist/images', false)
-    .copy('src/fontawesome', 'dist/webfonts', false)
-    .copy('src/fonts', 'dist/fonts', false)
-    .setPublicPath('dist');
+    .copy('src/html', 'build/html', false)
+    .copy('src/images', 'build/images', false)
+    .copy('src/fontawesome', 'build/webfonts', false)
+    .copy('src/fonts', 'build/fonts', false)
+    .setPublicPath('build');
