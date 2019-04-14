b667b50d7c92e54e47200585b628400021f65260
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-04-14 16:52:21 +0300

    Updated test to reflect new changes

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 import Faker from './faker';
 import Server from './server';
 import { Saved } from './settings';
-import { events } from './flash';
+import { events, flash } from './flash';
 import Zip, { IZip } from './zip';
 import File from './file';
 
@@ -21,72 +21,80 @@ export default class Test {
             let zipPath = faker.zip();
 
             // Start server if not started
-            let server = new Server(Saved.port);
+            new Server(Saved.port).start().then(server => {
+                // Make POST request to itself with zip (if fail then provide a random token)
+                let address = `http://127.0.0.1:${Saved.port}`;
 
-            // Make POST request to itself with zip (if fail then provide a random token)
-            let address = `127.0.0.1:${Saved.port}`;
+                let zip = fs.readFileSync(zipPath);
 
-            let zip = fs.readFileSync(zipPath);
+                let form = new FormData();
+                form.append('zip', zip, {
+                    filepath: zipPath,
+                    contentType: 'application/zip',
+                });
 
-            let form = new FormData();
-            form.append('zip', zip, {
-                filepath: zipPath,
-                contentType: 'application/zip',
+                form.append('accessToken', fail ? faker.slug() : Saved.accessToken);
+
+                axios.post(address, form, {
+                    headers: form.getHeaders(),
+                }).then(response => {
+                    if (fail) {
+                        assert.fail(response.data);
+                    } else {
+                        assert.strictEqual(response.data, '200');
+                    }
+                }).catch(error => {
+                    if (fail) {
+                        console.info(error);
+                    } else {
+                        assert.fail(error);
+                    }
+                });
+
+                // If set to fail check that the zip got deleted
+                events.$once('server-data-rejected', (accessToken: string, zip: IZip) => {
+                    console.log(accessToken, zip);
+                    assert.notStrictEqual(accessToken, Saved.accessToken);
+
+                    assert.strictEqual(fs.existsSync(zip.path), false);
+
+                    server.stop();
+                });
+
+                events.$once('server-data-recieved', (accessToken: string, zip: IZip) => {
+                    assert.strictEqual(accessToken, Saved.accessToken);
+
+                    // If fail is false then check that the zip got extracted
+                    assert.strictEqual(
+                        fs.existsSync(
+                            Zip.unpackDirectory(zip.path, File.filenameWithExtension(Saved.game.executable))
+                        ),
+                        true
+                    );
+
+                    server.stop();
+                });
+
+                // Check if the game got launched with the right params
+
+                // Check if the RPT is getting read by providing bogus build failed texts
+
+                // Check for cleanup of zip
+
+                // Check that build failed
             });
-
-            form.append('accessToken', fail ? faker.slug() : Saved.accessToken);
-
-            axios.post(address, form, {
-                headers: form.getHeaders(),
-            }).then(response => {
-                if (fail) {
-                    assert.fail(response.data);
-                } else {
-                    assert.strictEqual(response.data, '200');
-                }
-            }).catch(error => {
-                if (fail) {
-                    console.info(error);
-                } else {
-                    assert.fail(error);
-                }
-            });
-
-            // If set to fail check that the zip got deleted
-            events.$once('server-data-rejected', (accessToken: string, zip: IZip) => {
-                assert.notStrictEqual(accessToken, Saved.accessToken);
-
-                assert.strictEqual(fs.existsSync(zip.path), false);
-            });
-
-            events.$once('server-data-recieved', (accessToken: string, zip: IZip) => {
-                assert.strictEqual(accessToken, Saved.accessToken);
-
-                // If fail is false then check that the zip got extracted
-                assert.strictEqual(
-                    fs.existsSync(
-                        Zip.unpackDirectory(zip.path, File.filenameWithExtension(Saved.game.executable))
-                    ),
-                    true
-                );
-            });
-
-            // Check if the game got launched with the right params
-
-            // Check if the RPT is getting read by providing bogus build failed texts
-
-            // Check for cleanup of zip
-
-            // Check that build failed
-
-            server.stop();
         } catch (error) {
             didSucceed = false;
 
             if (! (error instanceof assert.AssertionError)) {
+                flash('Something went wrong during testing!', 'danger', true);
                 throw error;
             }
+
+            flash('The test failed!', 'danger', true);
         }
+
+        if (didSucceed) flash('The test succeeded!');
 
         // Return true if all went well
         return didSucceed;
