0cdd666a782a4137754d74ecd1ad446b2aa5f31b
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-04-16 13:41:17 +0300

    Removed wrong comment

--- src/ts/test.ts
+++ src/ts/test.ts
@@ -21,94 +21,93 @@ export default class Test {
 
     static requests(fail?: false): Promise<boolean> {
         return new Promise(async (resolve, reject) => {
-        let didSucceed = true;
+            let didSucceed = true;
 
-        try {
-            let faker = new Faker();
+            try {
+                let faker = new Faker();
 
-            // Generate some zip file
+                // Generate some zip file
                 let zipPath = await faker.zip();
 
-            // Start server if not started
-            new Server(Saved.port).start().then(server => {
-                // Make POST request to itself with zip (if fail then provide a random token)
-                let address = `http://127.0.0.1:${Saved.port}`;
+                // Start server if not started
+                new Server(Saved.port).start().then(server => {
+                    // Make POST request to itself with zip (if fail then provide a random token)
+                    let address = `http://127.0.0.1:${Saved.port}`;
 
-                let zip = fs.readFileSync(zipPath);
+                    let zip = fs.readFileSync(zipPath);
 
-                let form = new FormData();
+                    let form = new FormData();
 
-                // @ts-ignore Buffer is compatible with Blob because it's the same data structure
-                form.append('zip', new Blob(zip));
+                    form.append('zip', new Blob(zip));
 
-                form.append('accessToken', fail ? faker.slug() : Saved.accessToken);
+                    form.append('accessToken', fail ? faker.slug() : Saved.accessToken);
 
-                axios.post(address, form, {
-                    headers: {
-                        'content-type': 'multipart/form-data; boundary=' + this.generateBoundary()
-                    },
-                }).then(response => {
-                    if (fail) {
-                        assert.fail(response.data);
-                    } else {
-                        assert.strictEqual(response.data, 200);
-                    }
-                }).catch(error => {
-                    if (fail) {
-                        console.info(error);
-                    } else {
-                        assert.fail(error);
-                    }
+                    axios.post(address, form, {
+                        headers: {
+                            'content-type': 'multipart/form-data; boundary=' + this.generateBoundary()
+                        },
+                    }).then(response => {
+                        if (fail) {
+                            assert.fail(response.data);
+                        } else {
+                            assert.strictEqual(response.data, 200);
+                        }
+                    }).catch(error => {
+                        if (fail) {
+                            console.info(error);
+                        } else {
+                            assert.fail(error);
+                        }
+                    });
+
+                    // If set to fail check that the zip got deleted
+                    events.$on('server-data-rejected', (accessToken: string, zip: IZip) => {
+                        assert.notStrictEqual(accessToken, Saved.accessToken);
+
+                        // Need a delay because the file is removed in Processor after some time
+                        setTimeout(() => {
+                            assert.strictEqual(fs.existsSync(zip.path), false);
+                        }, 4 * 1000);
+
+                        server.stop();
+                    });
+
+                    events.$on('server-data-received', (accessToken: string, zip: IZip) => {
+                        assert.strictEqual(accessToken, Saved.accessToken);
+
+                        // If fail is false then check that the zip got extracted
+                        assert.strictEqual(
+                            fs.existsSync(
+                                Zip.unpackDirectory(zip.path, File.filenameWithExtension(Saved.game.executable))
+                            ),
+                            true
+                        );
+
+                        server.stop();
+                    });
+
+                    // Check if the game got launched with the right params
+
+                    // Check if the RPT is getting read by providing bogus build failed texts
+
+                    // Check for cleanup of zip
+
+                    // Check that build failed
                 });
+            } catch (error) {
+                didSucceed = false;
 
-                // If set to fail check that the zip got deleted
-                events.$on('server-data-rejected', (accessToken: string, zip: IZip) => {
-                    assert.notStrictEqual(accessToken, Saved.accessToken);
-
-                    // Need a delay because the file is removed in Processor after some time
-                    setTimeout(() => {
-                        assert.strictEqual(fs.existsSync(zip.path), false);
-                    }, 4 * 1000);
-
-                    server.stop();
-                });
-
-                events.$on('server-data-received', (accessToken: string, zip: IZip) => {
-                    assert.strictEqual(accessToken, Saved.accessToken);
-
-                    // If fail is false then check that the zip got extracted
-                    assert.strictEqual(
-                        fs.existsSync(
-                            Zip.unpackDirectory(zip.path, File.filenameWithExtension(Saved.game.executable))
-                        ),
-                        true
-                    );
-
-                    server.stop();
-                });
-
-                // Check if the game got launched with the right params
-
-                // Check if the RPT is getting read by providing bogus build failed texts
-
-                // Check for cleanup of zip
-
-                // Check that build failed
-            });
-        } catch (error) {
-            didSucceed = false;
-
-            if (! (error instanceof assert.AssertionError)) {
-                flash('Something went wrong during testing!', 'danger', true);
+                if (! (error instanceof assert.AssertionError)) {
+                    flash('Something went wrong during testing!', 'danger', true);
                     reject(error);
+                }
+
+                flash('The test failed!', 'danger', true);
             }
 
-            flash('The test failed!', 'danger', true);
-        }
+            if (didSucceed) flash('The test succeeded!');
 
-        if (didSucceed) flash('The test succeeded!');
-
-        // Return true if all went well
+            // Return true if all went well
             resolve(didSucceed);
         });
     }
