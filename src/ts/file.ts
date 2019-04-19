f0ca9ca6549d5936e307eb971a1bdb7193b3d0aa
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-04-19 20:36:02 +0300

    Added the ability to convert base64 to Blob

--- src/ts/file.ts
+++ src/ts/file.ts
@@ -13,4 +13,15 @@ export default class File {
     static directoryFromFilepath(filepath: string): string {
         return _.join(path.join(filepath, '../').split(path.sep), path.sep);
     }
+
+    static base64ToBlob(base64: string, mime: string): Blob {
+        let byteString = atob(base64);
+
+        let intArray = new Uint8Array(byteString.length);
+        for (let i = 0; i < byteString.length; i++) {
+            intArray[i] = byteString.charCodeAt(i);
+        }
+
+        return new Blob([intArray], { type: mime });
+    }
 }
