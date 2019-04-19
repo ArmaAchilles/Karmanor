8ae5174b1ba9e2d6e66a073605b8fb0ad4d8ea4c
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-04-19 23:49:31 +0300

    Added structure for Broadcast class

---
+++ src/ts/broadcast.ts
@@ -0,0 +1,11 @@
+import Build from "./build";
+
+export default class Broadcast {
+    static toDiscord(build: Build) {
+        //
+    }
+
+    static toSlack(build: Build) {
+        //
+    }
+}
