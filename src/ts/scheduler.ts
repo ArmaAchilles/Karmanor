6d8c9a534f0d7f515ff282b4f013432b8cee90cf
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-04-20 01:02:30 +0300

    Added Scheduler progression

--- src/ts/scheduler.ts
+++ src/ts/scheduler.ts
@@ -1,12 +1,17 @@
 import * as _ from 'lodash';
 
-import Build from "./build";
+import Build, { EBuildStatus } from "./build";
 
 export default class Scheduler {
     private builds: Build[];
+    private currentBuild: Build;
 
     add(build: Build): void {
         this.builds.push(build);
+
+        if (! this.currentBuild) {
+            this.setCurrentBuild(build);
+        }
     }
 
     remove(build: Build) {
@@ -15,5 +20,29 @@ export default class Scheduler {
 
     getAll(): Build[] {
         return this.builds;
+    }
+
+    private clearCurrentBuild(): void {
+        this.remove(this.currentBuild);
+        this.currentBuild = undefined;
+    }
+
+    private setCurrentBuild(build: Build): void {
+        this.currentBuild = build;
+
+        build.start().then(() => {
+            this.clearCurrentBuild();
+            this.setNextCurrentBuild();
+        });
+    }
+
+    private setNextCurrentBuild(): void {
+        if (this.builds) {
+            let pendingBuild = _.find(this.builds, (build: Build) => {
+                return build.getStatus() === EBuildStatus.pending;
+            });
+
+            this.setCurrentBuild(pendingBuild);
+        }
     }
 }
