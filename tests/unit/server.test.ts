55e0c1990510370532d4bd6f8416e92ef8b761b6
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-05-11 23:58:09 +0300

    Changed port to test Mechilles

--- tests/unit/server.test.ts
+++ tests/unit/server.test.ts
@@ -1,7 +1,7 @@
 import Server from '../../src/ts/server';
 
 test('Port is assigned in the class constructor', () => {
-    const port = 2567;
+    const port = 2566;
 
     const server = new Server(port);
 
