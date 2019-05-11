2a8cad9a59ad633e66a01f2ef8372be7bab3e720
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-05-11 15:03:41 +0300

    Mocks are no longer auto cleared

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-    // Automatically clear mock calls and instances between every test
-    clearMocks: true,
-
     // Indicates whether the coverage information should be collected while executing the test
     collectCoverage: true,
 
