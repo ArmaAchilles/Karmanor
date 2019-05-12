d68a7513aaf6680fbd9ca956475af91c5d79025a
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-05-12 23:36:50 +0300

    Removed unnecessary class Test replaced by Jest

--- src/ts/views/home-view.vue
+++ src/ts/views/home-view.vue
@@ -3,7 +3,5 @@
         <home-chart-component></home-chart-component>
 
         <server-control-component></server-control-component>
-
-        <debugger-component></debugger-component>
     </div>
 </template>
