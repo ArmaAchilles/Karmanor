578e0f2e83289d533fd4884d4a0f96c1dfee2ec6
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-05-12 11:02:47 +0300

    Removed unused classes

--- src/ts/listener.ts
+++
@@ -1,9 +0,0 @@
-import * as _ from 'lodash';
-
-export default class Listener<T> {
-    public callback: (eventArgs: T) => void;
-
-    constructor(callback: (eventArgs: T) => void) {
-        this.callback = callback;
-    }
-}
